0cdc6b3871c433dea2d1215407f6e49a7a3be3b3
omarbadawiii-91 <omarbadawe18@gmail.com>
2026-02-18 21:42:05 +0200

    Stripe_Gateway

--- lib/data/repo/services/api_keys.dart
+++ lib/data/repo/services/api_keys.dart
@@ -1,8 +1,8 @@
 class ApiKeys {
   static String token =
-      "sk_test_51SzYO36ZZ9UVgcr7vdcW0yVFqTkpzD1kXIZLbIgu60h5LmX3YHMRDKJS0ftOO3sb6q9CtrV5WIVB6B53RTOmFLEr00H3kUrfq0";
+      "";
   static String merchentdispalyname = "Omar";
   static String publishKey =
-      "pk_test_51SzYO36ZZ9UVgcr7i9SLQdpd5CDJMPgcq7rnyg6PLiDBCSjD7uOand9xp4xCKiKoHdUXDf2s8hseI5GzWgmwwAfN00zCJ2FsuB";
-  static String customid = "pm_1T2CSm6ZZ9UVgcr73mpaO4Nr";
+      "";
+  static String customid = "";
 }
